8607598d8cbcf9c3d76855b905b7ebd0d2d1a528
Caio Guilherme <caiobalbino@tce.mt.gov.br>
2025-09-30 12:45:13 -0400

    Create ExpenseController.java

---
+++ src/main/java/com/example/pagae_app/controllers/ExpenseController.java
@@ -0,0 +1,12 @@
+package com.example.pagae_app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+
+
+}
