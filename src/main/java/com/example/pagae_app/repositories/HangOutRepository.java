2203ee6bd4d98043e50d3ebcf5483dbea0de970d
Caio Guilherme <caiobalbino@tce.mt.gov.br>
2025-09-30 10:03:57 -0400

    Adicionado método para buscar HangOuts por usuário

--- src/main/java/com/example/pagae_app/repositories/HangOutRepository.java
+++ src/main/java/com/example/pagae_app/repositories/HangOutRepository.java
@@ -2,6 +2,12 @@ package com.example.pagae_app.repositories;
 
 import com.example.pagae_app.domain.hangout.HangOut;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HangOutRepository extends JpaRepository<HangOut, Long> {
+    @Query("SELECT hm.hangOut FROM hangout_members hm WHERE hm.user.id = :userId")
+    List<HangOut> findHangOutsByUserId(@Param("userId") Long userId);
 }
