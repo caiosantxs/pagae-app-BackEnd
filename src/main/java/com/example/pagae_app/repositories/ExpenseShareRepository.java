213ebe783daec97905737b69962c67a2787ed982
Caio Guilherme <caiobalbino@tce.mt.gov.br>
2025-10-06 13:30:40 -0400

    Update ExpenseShareRepository.java

--- src/main/java/com/example/pagae_app/repositories/ExpenseShareRepository.java
+++ src/main/java/com/example/pagae_app/repositories/ExpenseShareRepository.java
@@ -1,7 +1,12 @@
 package com.example.pagae_app.repositories;
 
 import com.example.pagae_app.domain.expense_shares.ExpenseShare;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpenseShareRepository extends JpaRepository<ExpenseShare, Long> {
+    Page<ExpenseShare> findExpenseShareByUserId(Long userId, Pageable pageable);
+
+    Page<ExpenseShare> findByExpense_HangOut_Id(Long expenseHangOutId, Pageable pageable);
 }
