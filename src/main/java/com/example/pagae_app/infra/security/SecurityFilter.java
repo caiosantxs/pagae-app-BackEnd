04778f955c63484f51d5cee396fe03106e59b6ce
Caio Guilherme <caiobalbino@tce.mt.gov.br>
2026-02-05 08:54:18 -0400

    fix: user is null após o registro de novo usuário

--- src/main/java/com/example/pagae_app/infra/security/SecurityFilter.java
+++ src/main/java/com/example/pagae_app/infra/security/SecurityFilter.java
@@ -25,12 +25,18 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if (token != null) {
-            var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByLogin(login);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        if(token != null){
+            var login = tokenService.validateToken(token);
+
+            if(login != null){
+                UserDetails user = userRepository.findByLogin(login);
+
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
         }
         filterChain.doFilter(request, response);
     }
